feaa8b1a23bf7d58b49b48943087a02ee9ab45c2
Nick Davies <nickdavies07@outlook.com>
2019-03-05 14:26:29 +0000

    Test a user can be stored
    Tests a new user can be created and stored in the database. Creates a
    new role and a new user and assigns the role to the user. Asserts that
    the database has a user with that user ID, role ID, name and email
    address.

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Role;
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+	use RefreshDatabase;
+
+    /*
+     * Test a new User can be stored
+     */
+    public function test_a_user_can_be_stored()
+	{
+		$role = factory(Role::class)->create();
+
+		$user = factory(User::class)->create([
+			'role_id' => $role->id,
+			'name' => 'Test User',
+			'email' => 'test_user@test.com'
+		]);
+
+		$this->assertDatabaseHas('users', [
+			'id' => $user->id,
+			'role_id' => $role->id,
+			'name' => 'Test User',
+			'email' => 'test_user@test.com'
+		]);
+	}
+}
