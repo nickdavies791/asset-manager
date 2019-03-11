dfee0224af541303526c83b15567ae4884c3cad4
Nick Davies <nickdavies07@outlook.com>
2019-03-11 12:56:21 +0000

    Move foreign keys into Schema::table
    Moves foreign keys for school_id and user_id into a Schema::table method
    for cleanliness.

--- database/migrations/2019_03_06_102553_create_school_user_table.php
+++ database/migrations/2019_03_06_102553_create_school_user_table.php
@@ -17,9 +17,12 @@ class CreateSchoolUserTable extends Migration
             $table->bigIncrements('id');
             $table->unsignedBigInteger('school_id');
             $table->unsignedBigInteger('user_id');
-            $table->foreign('school_id')->references('id')->on('schools');
-            $table->foreign('user_id')->references('id')->on('users');
         });
+
+		Schema::table('school_user', function (Blueprint $table) {
+			$table->foreign('school_id')->references('id')->on('schools');
+			$table->foreign('user_id')->references('id')->on('users');
+		});
     }
 
     /**
