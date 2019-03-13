bdca5d211986a7865bc5afcfe515a57e7c6bcaa7
Nick Davies <nickdavies07@outlook.com>
2019-03-13 13:49:20 +0000

    Test validation when updating types
    Tests validation when updating asset types. Asserts an error message is
    returned if validation fails.

--- tests/Feature/Type/UpdateTypeValidationTest.php
+++ tests/Feature/Type/UpdateTypeValidationTest.php
@@ -3,38 +3,43 @@
 namespace Tests\Feature;
 
 use App\Role;
+use App\Type;
 use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class StoreTypeValidationTest extends TestCase
+class UpdateTypeValidationTest extends TestCase
 {
 	use RefreshDatabase;
 	use WithFaker;
 
 	/*
-	 * Test the name field is required
+	 * Test name field is required
 	 */
 	public function test_name_field_is_required()
 	{
 		$role = factory(Role::class)->create(['name' => 'Administrator']);
 		$user = factory(User::class)->create(['role_id' => $role->id]);
+		$type = factory(Type::class)->create();
 
-		$response = $this->actingAs($user)->post(route('types.store'));
+		$response = $this->actingAs($user)->put(route('types.update', ['id' => $type->id]));
 		$response->assertSessionHasErrors('name');
 		$this->assertEquals(session('errors')->get('name')[0], 'Please enter a name for the asset type');
 	}
 
 	/*
-	 * Test the name field is of type string
+	 * Test name field is of type string
 	 */
 	public function test_name_field_is_string()
 	{
 		$role = factory(Role::class)->create(['name' => 'Administrator']);
 		$user = factory(User::class)->create(['role_id' => $role->id]);
+		$type = factory(Type::class)->create();
 
-		$response = $this->actingAs($user)->post(route('types.store'), ['name' => $this->faker->randomNumber()]);
+		$response = $this->actingAs($user)->put(route('types.update', ['id' => $type->id]), [
+			'name' => $this->faker->randomNumber()
+		]);
 		$response->assertSessionHasErrors('name');
 		$this->assertEquals(session('errors')->get('name')[0], 'The name field must be a string');
 	}
