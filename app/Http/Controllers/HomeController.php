3d0f9618ab6cd6eccae96647c3f72a31552c4292
Nick Davies <nickdavies07@outlook.com>
2019-03-13 15:19:01 +0000

    Add authenticated users schools to home
    Adds the currently authenticated users schools to the home blade file.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+    	$schools = auth()->user()->schools;
+
+        return view('home')->with('schools', $schools);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,11 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    @forelse($schools as $school)
+                        <a href="{{ route('schools.show', ['id' => $school->id]) }}">{{ $school->name }}</a><br />
+                    @empty
+                        You are not associated with any schools.
+                    @endforelse
                 </div>
             </div>
         </div>
