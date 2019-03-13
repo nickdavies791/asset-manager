835d2d9dc2973c30f8123f21bafbe4c1e1c1a965
Nick Davies <nickdavies07@outlook.com>
2019-03-13 13:45:36 +0000

    Test validation when updating schools
    Tests validation is checked when updating schools. Asserts error message
    is returned if validation fails.

---
+++ tests/Feature/School/UpdateSchoolValidationTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Role;
+use App\School;
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UpdateSchoolValidationTest extends TestCase
+{
+	use RefreshDatabase;
+	use WithFaker;
+
+	/*
+	 * Test the name field is required
+	 */
+	public function test_name_field_is_required()
+	{
+		$role = factory(Role::class)->create(['name' => 'Administrator']);
+		$user = factory(User::class)->create(['role_id' => $role->id]);
+		$school = factory(School::class)->create();
+
+		$response = $this->actingAs($user)->put(route('schools.update', ['id' => $school->id]));
+		$response->assertSessionHasErrors('name');
+		$this->assertEquals(session('errors')->get('name')[0], 'Please enter a name for the school');
+	}
+
+	/*
+	 * Test the name field is of type string
+	 */
+	public function test_name_field_is_string()
+	{
+		$role = factory(Role::class)->create(['name' => 'Administrator']);
+		$user = factory(User::class)->create(['role_id' => $role->id]);
+		$school = factory(School::class)->create();
+
+		$response = $this->actingAs($user)->put(route('schools.update', ['id' => $school->id]), [
+			'name' => $this->faker->randomNumber()
+		]);
+		$response->assertSessionHasErrors('name');
+		$this->assertEquals(session('errors')->get('name')[0], 'The name field must be a string');
+	}
+}
