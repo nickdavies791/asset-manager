5afdc42884e0a4b98633e21a438bc314213733ff
Nick Davies <nickdavies07@outlook.com>
2019-03-16 18:43:19 +0000

    Add button to create assets from home view
    Adds a new button linking to the assets.create route on the home view.
    Wraps button in can blade directive to check the auth user can create
    assets and shows/hides accordingly.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,9 @@
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
-
+                    @can('create', App\Asset::class)
+                        <a class="btn btn-primary" href="{{ route('assets.create') }}">Add Asset</a>
+                    @endcan
                 </div>
             </div>
         </div>
