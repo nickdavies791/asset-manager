3e2f463fec8cb4f43972d81e69bcd277e7811bcc
Nick Davies <nickdavies07@outlook.com>
2019-03-26 11:36:11 +0000

    Rename test filename
    Renames the SettingsAuthorizationTest to SettingsAuthenticationTest.

--- tests/Feature/Settings/SettingsAuthenticationTest.php
+++ tests/Feature/Settings/SettingsAuthenticationTest.php
@@ -8,7 +8,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class SettingsAuthorizationTest extends TestCase
+class SettingsAuthenticationTest extends TestCase
 {
 	use RefreshDatabase;
 
